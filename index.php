c9120b54f37b2a6d8aafe795a66a6f70fb7cef27
Supanut <dandelion28805@gmail.com>
2023-01-04 09:27:05 +0700

    change html

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+
+    <h1>This is a Heading</h1><br>
+    <h2>This is a Heading</h2>
+    <p>This is a paragraph.</p>
+    <a href="https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjN-P_k46z8AhWRTXwKHZW1CosQPAgI">Supanut</a><br>
+    <!-- <img src="https://www.thedrive.com/content/archive-images/lamborghini-egoista-electric-supercar-hero.jpg?auto=webp&optimize=high&quality=70&width=1920"> -->
+
+    <input type="text" id="input01"><br>
+
+    <!-- <button>submit</button><br> -->
+
+    <input type="text" id="input02"><br>
+
+    <button id="button01" onclick="checkinput()">Click!</button>
+</body>
+
+
+<script>
+    function checkinput(){
+        console.log('Hi Supanut')
+        // alert('Hello world')
+    }
+    
+</script>
+
+</html>
