fc5829632b41b83d9845c14ec726298805c5377b
techin <techin.naja@gmail.com>
2023-01-04 09:27:05 +0700

    test01

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+
+    <h1>This is a Heading</h1><br>
+    <h2>This is a Heading</h2>
+    <p>This is a paragraph.</p>
+    <a href="https://www.google.co.th/"> Techin S. </a> <br>
+
+    <!-- <img src="https://www.popsci.com/uploads/2021/06/15/lambo.jpeg"> -->
+
+    <input type="text" id="input1"> <br>
+    <input type="text" id="input2"> <br>
+    <button id="btn1" onclick="checkinput()" >submit</button>
+
+</body>
+
+<script>
+    function checkinput(){
+        console.log('Hi Techin')
+        // alert('Hello Techin')
+    }
+
+</script>
+
+</html>
